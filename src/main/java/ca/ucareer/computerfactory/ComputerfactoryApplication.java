9bc0c26c87d6f188e3019fb35c9d336c43a576d8
Jiangqi Li <jiangqi319@gmail.com>
2020-08-05 21:15:49 -0400

    init project

---
+++ src/main/java/ca/ucareer/computerfactory/ComputerfactoryApplication.java
@@ -0,0 +1,13 @@
+package ca.ucareer.computerfactory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComputerfactoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComputerfactoryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ca/ucareer/computerfactory/ComputerfactoryApplicationTests.java
@@ -0,0 +1,13 @@
+package ca.ucareer.computerfactory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComputerfactoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
