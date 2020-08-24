b5c0c8a0370e665af7b14a4d89a861d3cfe78370
tech2bits <47727045+tech2bits@users.noreply.github.com>
2020-08-24 18:32:16 -0400

    Rename Computer Entity.

--- src/main/java/ca/ucareer/computerfactory/computer/Computer.java
+++ src/main/java/ca/ucareer/computerfactory/computer/Computer.java
@@ -7,7 +7,7 @@ import javax.persistence.*;
 import java.util.Date;
 
 @Entity
-@Table(name = "Computer")
+@Table(name = "Computers")
 public class Computer {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
